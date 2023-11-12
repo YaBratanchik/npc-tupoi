24adc68227a154f9040c94fe075e774b7c294375
YaNeBratanchik <119120678+YaBratanchik@users.noreply.github.com>
2023-11-12 17:25:35 +0500

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //здоровье NPC
+    public int health = 5;
+
+    //уровень NPC
+    public int level = 1;
+
+    //скорость NPC
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
